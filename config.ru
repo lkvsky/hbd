94fd452907ad0cef81ce623593c1bbacb0e4330b
lkvsky <kyle@lucovsky.com>
2013-03-18 18:09:58 -0700

    adding confetti

--- config.ru
+++ config.ru
@@ -3,4 +3,5 @@ require 'newrelic_rpm'
 
 NewRelic::Agent.after_fork(:force_reconnect => true)
 NewRelic::Agent.manual_start :app_name => 'hbd-party'
+
 run Sinatra::Application
--- public/hbd.js
+++ public/hbd.js
@@ -1,13 +1,47 @@
 $(function() {
   var HBD = (function() {
+    function Confetti(ctx, x, y) {
+      var that = this;
+
+      that.x = x;
+      that.y = y;
+      that.DIM = 20;
+
+      that.draw = function() {
+        that.update({x: 0, y: 2});
+        that.drawSquare();
+      };
+
+      that.drawSquare = function() {
+        ctx.beginPath();
+        ctx.fillStyle = "#000";
+        ctx.rect(that.x, that.y, that.DIM, that.DIM);
+        ctx.fill();
+      };
+
+      that.update = function(velocity) {
+        that.x += velocity.x;
+        that.y += velocity.y;
+      };
+    }
+
     function PaintCanvas(ctx) {
       var that = this;
+
+      that.confetti = [];
 
       that.draw = function() {
         ctx.canvas.width = window.innerWidth;
         ctx.canvas.height = window.innerHeight;
         that.drawBackground();
         that.drawText();
+        that.drawConfetti()
+      };
+
+      that.drawConfetti = function() {
+        for (var i=0; i<that.confetti.length; i++) {
+          that.confetti[i].draw();
+        }
       };
 
       that.drawBackground = function() {
@@ -34,7 +68,20 @@ $(function() {
         return "rgb(" + rgb.join(", ") + ")";
       };
 
+      that.generateConfetti = function(clickedX, clickedY) {
+        c = new Confetti(ctx, clickedX, clickedY);
+
+        that.confetti.push(c);
+      };
+
+      that.clickPage = function() {
+        $("canvas").click(function(e) {
+          that.generateConfetti(e.pageX, e.pageY);
+        });
+      };
+
       that.initialize = (function() {
+        that.clickPage();
         setInterval(that.draw, 255);
       })();
     }
